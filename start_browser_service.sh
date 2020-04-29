e034df6fef2d3532e6e25751a60a1063fc9dea00
sseol11 <whiteldark@gmail.com>
2020-04-29 19:59:39 +0000

    change the port

--- start_browser_service.sh
+++ start_browser_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python parlai/chat_service/services/browser_chat/run.py    --config-path  parlai/chat_service/tasks/covid19_chatbot/config.yml >/dev/null 2>&1 &
+python parlai/chat_service/services/browser_chat/run.py    --config-path  parlai/chat_service/tasks/covid19_chatbot/config.yml --port 5002 >/dev/null 2>&1 &
 sleep 6
-python parlai/chat_service/services/browser_chat/client.py
+python parlai/chat_service/services/browser_chat/client.py --port 5002
