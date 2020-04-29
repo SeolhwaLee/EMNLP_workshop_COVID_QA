54baaf3f43bc3c2a82458f57983b2c52ba01c993
sseol11 <whiteldark@gmail.com>
2020-04-29 15:03:05 -0400

    Create start_browser_service.sh

---
+++ start_browser_service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python parlai/chat_service/services/browser_chat/run.py    --config-path  parlai/chat_service/tasks/covid19_chatbot/config.yml >/dev/null 2>&1 &
+sleep 6
+python parlai/chat_service/services/browser_chat/client.py
