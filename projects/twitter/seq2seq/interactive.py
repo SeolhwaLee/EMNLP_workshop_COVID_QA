d3296527d26c37dd0ddc55d3e93660adff5603aa
sseol11 <whiteldark@gmail.com>
2020-05-20 03:45:20 +0000

    for chateval script

--- projects/twitter/seq2seq/interactive.py
+++ projects/twitter/seq2seq/interactive.py
@@ -11,17 +11,45 @@ This seq2seq model was trained on convai2:self.
 
 from parlai.core.build_data import download_models
 from parlai.core.params import ParlaiParser
-from parlai.scripts.interactive import interactive
+from parlai.scripts.interactive import interactive, setup_args
 
 if __name__ == '__main__':
-    parser = ParlaiParser(add_model_args=True)
+    # parser_new = ParlaiParser(add_model_args=True)
+    parser = setup_args()
     parser.set_params(
         model='legacy:seq2seq:0',
         model_file='models:convai2/seq2seq/convai2_self_seq2seq_model',
         dict_file='models:convai2/seq2seq/convai2_self_seq2seq_model.dict',
         dict_lower=True,
     )
+    parser.add_argument(
+        '-sc',
+        '--script-chateval',
+        type='bool',
+        default=False,
+        dest='chat_script',
+        help='Chateval script read file'
+             'True: chateval evaluation, False: single-turn conversation with agent(original model)',
+    )
+    parser.add_argument(
+        '-scip',
+        '--chateval-input-path',
+        type=str,
+        default=None,
+        dest='script_input_path',
+        help='Chateval script input path',
+    )
+    parser.add_argument(
+        '-scop',
+        '--chateval-output-path',
+        type=str,
+        default=None,
+        dest='script_output_path',
+        help='Chateval result output path',
+    )
+
     opt = parser.parse_args()
+    # opt = parser.setup_args()
     if opt.get('model_file', '').startswith('models:convai2'):
         opt['model_type'] = 'seq2seq'
         fnames = [
@@ -30,4 +58,5 @@ if __name__ == '__main__':
             'convai2_self_seq2seq_model.opt',
         ]
         download_models(opt, fnames, 'convai2', version='v3.0')
+
     interactive(opt)
